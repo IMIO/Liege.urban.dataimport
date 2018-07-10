e284ee1a92164e4528efb660f5fdd7f884976179
Simon Delcourt <delcourt.simon@gmail.com>
2018-07-10 10:35:31 +0200

    Create the old rubric folder during products install.

--- src/Liege/urban/dataimport/setuphandlers.py
+++ src/Liege/urban/dataimport/setuphandlers.py
@@ -9,3 +9,25 @@ def post_install(context):
     """Post install script"""
     if isNotCurrentProfile(context): return
     portal = context.getSite()
+    addRubricValues(context)
+
+
+
+def addRubricValues(context):
+    site = context.getSite()
+    config_folder = site.portal_urban.rubrics
+
+    oldrubrics_folder_id = 'old_rubrics'
+    if oldrubrics_folder_id in config_folder.objectIds():
+        rubric_folder = getattr(config_folder, oldrubrics_folder_id)
+    else:
+        rubricfolder_id = config_folder.invokeFactory(
+            "Folder",
+            id=oldrubrics_folder_id,
+            title="Rubrique reprise de données"
+        )
+        rubric_folder = getattr(config_folder, rubricfolder_id)
+        rubric_folder.setConstrainTypesMode(1)
+        rubric_folder.setLocallyAllowedTypes(['EnvironmentRubricTerm'])
+        rubric_folder.setImmediatelyAddableTypes(['EnvironmentRubricTerm'])
+
